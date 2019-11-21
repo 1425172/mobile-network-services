8f6f0f91ad15e70fd1af0efe26ac0282c69c1679
Stefan Heigl <stefan.heigl90@gmail.com>
2019-11-21 15:57:01 +0100

    initial commit assignment 2

---
+++ assignment2/app/src/main/java/at/ac/tuwien/nsa/gr12/comparelocations/MainActivity.kt
@@ -0,0 +1,19 @@
+package at.ac.tuwien.nsa.gr12.comparelocations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import at.ac.tuwien.nsa.gr12.comparelocations.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ assignment2/app/src/main/java/at/ac/tuwien/nsa/gr12/comparelocations/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package at.ac.tuwien.nsa.gr12.comparelocations.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
