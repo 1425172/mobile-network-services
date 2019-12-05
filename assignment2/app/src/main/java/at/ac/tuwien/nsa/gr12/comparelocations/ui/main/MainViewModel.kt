9884e0cca17b6faaf48f27e0caa7b9d0cceb2a7a
Pirmin2 <e1527339@student.tuwien.ac.at>
2019-12-05 23:17:42 +0100

    Maybe implemented some kind of view model.

--- assignment2/app/src/main/java/at/ac/tuwien/nsa/gr12/comparelocations/ui/main/MainViewModel.kt
+++ assignment2/app/src/main/java/at/ac/tuwien/nsa/gr12/comparelocations/ui/main/MainViewModel.kt
@@ -1,7 +1,39 @@
 package at.ac.tuwien.nsa.gr12.comparelocations.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import at.ac.tuwien.nsa.gr12.comparelocations.core.model.Report
+import at.ac.tuwien.nsa.gr12.comparelocations.core.use.cases.ReportUseCase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val reportUseCase: ReportUseCase) : ViewModel() {
+
+    private lateinit var allReports: LiveData<List<Report>>
+
+    init {
+        GlobalScope.launch {
+            allReports = MutableLiveData<List<Report>>(reportUseCase.getAll())
+        }
+    }
+
     // TODO: Implement the ViewModel
+    init {
+        Log.i("Created Model","Created!")
+    }
+
+    fun getAllData(): LiveData<List<Report>> {
+        return allReports
+    }
+
+    suspend fun getNew(){
+        reportUseCase.getNew()
+    }
+
+    suspend fun remove(report: Report){
+        reportUseCase.remove(report)
+    }
+
 }
